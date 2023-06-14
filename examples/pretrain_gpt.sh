03535e906547e487bd586582db0ac0df1920a132
chengming-zhang <chengming.zhang@anl.gov>
2023-06-14 00:19:39 +0000

    alcf

--- examples/pretrain_gpt.sh
+++ examples/pretrain_gpt.sh
@@ -4,19 +4,45 @@
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-CHECKPOINT_PATH=<Specify path>
-VOCAB_FILE=<Specify path to file>/gpt2-vocab.json
-MERGE_FILE=<Specify path to file>/gpt2-merges.txt
-DATA_PATH=<Specify path and file prefix>_text_document
+CHECKPOINT_PATH="/home/czh5/genome/Megatron-LM/outputs"
+VOCAB_FILE="/home/czh5/genome/Megatron-LM/dataset/gpt2-vocab.json"
+MERGE_FILE="/home/czh5/genome/Megatron-LM/dataset/gpt2-merges.txt"
+DATA_PATH="/home/czh5/genome/Megatron-LM/dataset/BookCorpusDataset_text_document"
+
+# ┏━━━━━━━━━━━━━━━━━━━━┓
+# ┃ GPT-3 Small:  125M ┃
+# ┗━━━━━━━━━━━━━━━━━━━━┛
+# MODEL_SIZE="125M"
+# NLAYERS=12
+# HIDDEN=768
+# ATEN_HEADS=12
+# GLOBAL_BATCH=128
+
+# ┏━━━━━━━━━━━━━━━━━━━━┓
+# ┃ GPT-3: 1.5B Params ┃
+# ┗━━━━━━━━━━━━━━━━━━━━┛
+MODEL_SIZE="1.5B"
+NLAYERS=48
+HIDDEN=1536
+ATEN_HEADS=24
+GLOBAL_BATCH=64
+
+MICRO_BATCH=16
+SEQ_LEN=$((2*1024))
 
 GPT_ARGS="
-    --num-layers 24 \
-    --hidden-size 1024 \
-    --num-attention-heads 16 \
-    --seq-length 1024 \
-    --max-position-embeddings 1024 \
-    --micro-batch-size 4 \
-    --global-batch-size 8 \
+    --tensor-model-parallel-size 4 \
+    --pipeline-model-parallel-size 4 \
+    --sequence-parallel \
+    --use-flash-attn \
+    --recompute-activations \
+    --num-layers $NLAYERS \
+    --hidden-size $HIDDEN \
+    --num-attention-heads $ATEN_HEADS \
+    --seq-length $SEQ_LEN \
+    --max-position-embeddings $SEQ_LEN \
+    --micro-batch-size $MICRO_BATCH \
+    --global-batch-size $GLOBAL_BATCH \
     --lr 0.00015 \
     --train-iters 500000 \
     --lr-decay-iters 320000 \
@@ -43,7 +69,8 @@ OUTPUT_ARGS="
     --eval-iters 10
 "
 
-torchrun pretrain_gpt.py \
+# torchrun pretrain_gpt.py \
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=12346 pretrain_gpt.py \
     $GPT_ARGS \
     $DATA_ARGS \
     $OUTPUT_ARGS \
